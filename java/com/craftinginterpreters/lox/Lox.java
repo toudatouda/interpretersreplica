5f982098bc2660fbc59d4877cac7399971cfab53
LiuHesheng <lhsokgood@163.com>
2024-03-17 16:25:59 +0800

    2023/3/17/ThinkBook

--- java/com/craftinginterpreters/lox/Lox.java
+++ java/com/craftinginterpreters/lox/Lox.java
@@ -48,7 +48,10 @@ public class Lox {
   private static void run(String source) {
     Scanner scanner = new Scanner(source);
     List<Token> tokens = scanner.scanTokens();
-
+    Parser parser = new Parser(tokens);
+    Expr expression = parser.parse();
+    if (hadError) return;
+    System.out.println(new AstPrinter().print(expression));
     for (Token token : tokens) {
       System.out.println(token);
     }
@@ -63,5 +66,13 @@ public class Lox {
     System.err.println(
         "[line " + line + "] Error" + where + ": " + message);
         hadError = true;
+  }
+
+  static void error(Token token, String message) {
+    if (token.type == TokenType.EOF) {
+      report(token.line, " at end", message);
+    } else {
+      report(token.line, " at '" + token.lexeme + "'", message);
+    }
   }
 }
